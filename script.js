5d767b12468b5f31293fe9160ab1c443f8865eb6
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-24 15:49:40 +0100

    Add files via upload
    Module 3, Level 14: JavaScript calling functions in functions

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+const makePositive = function(number) {
+   console.log("Entering makePositive..");
+   if (number < 0) {
+       return Math.abs(number);
+   }
+   return number;
+};
+
+const makeBigger = function(number) {
+   console.log("Entering makeBigger..");
+   if (number < 10) {
+       return 10 + number;
+   }
+   return number;
+};
+
+const square = function(number) {
+   console.log("Entering square..");
+   return number * number;
+};
+
+const doComplexCalculation = function(number) {
+   console.log("Entering doComplexCalculation..");
+   const positiveNumber = makePositive(number);
+   const bigNumber = makeBigger(positiveNumber);
+   const squaredNumber = square(bigNumber);
+   return squaredNumber;
+};
+
+console.log(doComplexCalculation(-1));
+*/
+
+const person = function (age) {
+   if (age >= 18) {
+      return true;
+   }
+}
+
+const 
